9e15738ac2ccd7d664652320ae585228d7c7aeb1
EerieEight <80974420+EerieEight@users.noreply.github.com>
2022-02-20 18:39:00 -0500

    approve mcv2 for dummy

--- scripts/deploy_token.ts
+++ scripts/deploy_token.ts
@@ -1,6 +1,7 @@
 import { ethers, run } from "hardhat"
 
 async function main() {
+    const masterchefv2 = ""
     const Token = await ethers.getContractFactory("ERC20Mock");
     const token = await Token.deploy("dummy", "dummy", 1000);
     await token.deployed()
@@ -11,6 +12,9 @@ async function main() {
         address: token.address,
         constructorArguments: ["dummy", "dummy", 1000],
     })
+
+    let tx = await token.approve(masterchefv2, 1000)
+    console.log(tx)
   }
   
   main()

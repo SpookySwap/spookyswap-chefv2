63912fbdc989735de6e6425ffffdf55f57fd162e
0xALUCARD <0xALUCARD@protonmail.com>
2022-05-23 23:35:14 +0200

    admin spookyauth

--- contracts/SpookyAuth.sol
+++ contracts/SpookyAuth.sol
@@ -5,19 +5,38 @@ abstract contract SpookyAuth {
     // set of addresses that can perform certain functions
     mapping(address => bool) public isAuth;
     address[] public authorized;
+    address admin;
 
     modifier onlyAuth() {
-        require(isAuth[msg.sender], "SpookySwap: FORBIDDEN");
+        require(isAuth[msg.sender] || msg.sender == admin, "SpookySwap: FORBIDDEN (auth)");
+        _;
+    }
+
+    modifier onlyOwner() { //Ownable compatibility
+        require(isAuth[msg.sender] || msg.sender == admin, "SpookySwap: FORBIDDEN (auth)");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "SpookySwap: FORBIDDEN (admin)");
         _;
     }
 
     event AddAuth(address indexed by, address indexed to);
     event RevokeAuth(address indexed by, address indexed to);
+    event SetAdmin(address indexed by, address indexed to);
 
     constructor() {
+        admin = msg.sender;
+        emit SetAdmin(address(this), msg.sender);
         isAuth[msg.sender] = true;
         authorized.push(msg.sender);
         emit AddAuth(address(this), msg.sender);
+    }
+
+    function setAdmin(address newAdmin) external onlyAdmin {
+        admin = newAdmin;
+        emit SetAdmin(msg.sender, newAdmin);
     }
 
     function addAuth(address _auth) external onlyAuth {
@@ -27,6 +46,7 @@ abstract contract SpookyAuth {
     }
 
     function revokeAuth(address _auth) external onlyAuth {
+        require(_auth != admin);
         isAuth[_auth] = false;
         emit RevokeAuth(msg.sender, _auth);
     }
